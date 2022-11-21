bcb197c5c785bb8441b66bfde3561c6c5027a22e
giorgi <gcuxishvili@gmail.com>
2022-11-21 15:03:13 +0400

    feat: create command to request data

---
+++ database/migrations/2022_11_21_102016_create_countries_data_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countries_data', function (Blueprint $table) {
+			$table->id();
+			$table->text('name');
+			$table->integer('confirmed');
+			$table->integer('recovered');
+			$table->integer('deaths');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('countries_data');
+	}
+};
