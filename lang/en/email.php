176bd6a15507f4e4154d2bbcd4b66e9faeacfc3f
giorgi <gcuxishvili@gmail.com>
2022-11-25 15:49:01 +0400

    feat: add email verification template and logic
    I added email verification tempalte which customer will receive and I also implemented logic that will verify email

---
+++ lang/en/email.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'confirmation'     => 'Confirmation email',
+	'click-button'     => 'click this button to verify your email',
+	'verify-email'     => 'Verify email',
+	'account-verified' => 'Your account is confirmed, you can sign in',
+	'sign-in'          => 'Sign in',
+];
