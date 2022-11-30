4e95ccbae6edeff8df7dc78706406004e228b3f3
giorgi <gcuxishvili@gmail.com>
2022-11-30 15:04:51 +0400

    chore: add coronatime to request data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('command:request-data')->daily();
+		$schedule->command('command:coronatime-request-data')->daily();
 	}
 
 	protected function commands()
